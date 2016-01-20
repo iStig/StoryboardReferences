df6dc5143e814b15278443e848894d38eee0f871
iStig <istig@iStigdeMacBook-Pro.local>
2016-01-20 11:40:28 +0800

    push first

---
+++ StoryboardReferences/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  StoryboardReferences
+//
+//  Created by Chris Grant on 24/06/2015.
+//  Copyright © 2015 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ StoryboardReferences/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  StoryboardReferences
+//
+//  Created by Chris Grant on 24/06/2015.
+//  Copyright © 2015 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
